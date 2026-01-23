733ed608f23127c326a6b096b4f18941153eb848
Pramodya De Alwis <pramodya@spiceprotocol.xyz>
2026-01-23 16:08:10 +1300

    chore: remove unwanted import

--- src/interfaces/IExampleCoverageAgent.sol
+++ src/interfaces/IExampleCoverageAgent.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {ICoverageAgent, ClaimCoverageRequest} from "./ICoverageAgent.sol";
+import {ClaimCoverageRequest} from "./ICoverageAgent.sol";
 import {CoverageClaimStatus} from "./ICoverageProvider.sol";
 
 /// @title IExampleCoverageAgent
